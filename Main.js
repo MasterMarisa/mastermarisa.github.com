18981299957d74ff3da467e71942a4ff85d6a1c8
MasterMarisa <it_huea@163.com>
2023-10-15 11:10:37 +0800

    first commit

---
+++ Main.js
@@ -0,0 +1,23 @@
+﻿Timer();
+setInterval(Timer,1000);
+Timer();
+let Surprise = () => {
+    document.write('<img style="width:40%;height:auto;margin-left:30%;" src="./恶心丸.webp"></img>');
+    alert(a);
+}
+function Timer(){
+    let i = Date();
+    i = i.slice(0,i.indexOf("+")-3);
+    document.getElementById("clock").innerHTML=i;
+}
+
+function AddAduio(Name,Artist,Url,Cover,Lrc="none"){
+    ap.list.add([{
+        name:Name,
+        artist:Artist,
+        url:Url,
+        cover:Cover,
+        lrc:Lrc
+    },]);
+}
+
